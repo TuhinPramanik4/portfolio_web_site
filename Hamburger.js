0ce5e2be4f6c0af29b6af5bdefbb655aafdd5882
Tuhin Pramanik <156042548+TuhinPramanik4@users.noreply.github.com>
2024-07-18 22:30:33 -0700

    Add files via upload

---
+++ Hamburger.js
@@ -0,0 +1,8 @@
+function showHamburger(){
+    const hamburger=document.querySelector('.hamburger');
+    hamburger.style.display ='flex' ;  
+     }
+function HideIt(){
+        const hamburger=document.querySelector('.hamburger');
+    hamburger.style.display ='none' ;
+       }
